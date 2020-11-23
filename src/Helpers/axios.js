3b818b02301645877c8a86fc5ec78d94571444da
Natascha Hof <68080188+Natascha2020@users.noreply.github.com>
2020-11-23 20:38:34 +0100

    Update axios.js

--- src/Helpers/axios.js
+++ src/Helpers/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
   validateStatus: () => true,
-  withCredentials: true,
+  withCredentials: "include",
 });
 
 export default axiosInstance;
